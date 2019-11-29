e31ba39b082e972b84fbbffabc9e1bb80679b54b
pluto <pluto@eastandroid.com>
2019-11-29 18:32:26 +0900

    simple test add

---
+++ operaxinterceptor/src/androidTest/java/dev/eastar/operaxinterceptor/event/OperaXEventTest.java
@@ -0,0 +1,16 @@
+package dev.eastar.operaxinterceptor.event;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class OperaXEventTest {
+    @Test
+    public void sendTest() {
+        OperaXEventObservable.notify(OperaXEvents.EXIT);
+        OperaXEventObservable.notify(new LOGIN("dfsdfs"));
+        OperaXEventObservable.notify(new LOGIN());
+    }
+}
