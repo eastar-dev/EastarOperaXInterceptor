45962c04b153f0cae1ed5231088f98590f0c17dc
eastarj <eastarj@gmail.com>
2019-12-03 01:57:23 +0900

    no message

--- operaxinterceptor/src/androidTest/java/dev/eastar/operaxinterceptor/event/OperaXEventTestKt.kt
+++ operaxinterceptor/src/androidTest/java/dev/eastar/operaxinterceptor/event/OperaXEventTestKt.kt
@@ -8,7 +8,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class OperaXEventTestKt {
+class  OperaXEventTestKt {
     @Test
     fun useAppContext() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
